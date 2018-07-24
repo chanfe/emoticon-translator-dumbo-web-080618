02b5d6df04d283d3903ea8aaa25e5b6dee31121d
Felix Chan <felixchan29@gmail.com>
2018-07-24 02:47:31 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,22 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+
+def load_library(path)
+  # code goes here emoticons.yml
+  new_hash = {}
+  emo_hash = YAML.load_file(path)
+  emo_hash.each do |key, value|
+    
+  end
+  binding.pry
+end
+
+def get_japanese_emoticon(path, emo)
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
+def get_english_meaning(path, emo)
   # code goes here
 end
